fb2365a7aa1fcfb35c89d87123abda9fadaab7e0
Amyoyzl <1421790940@qq.com>
2019-07-16 20:09:23 +0800

    add company and employee in model

---
+++ src/main/java/com/tw/apistackbase/model/Company.java
@@ -0,0 +1,42 @@
+package com.tw.apistackbase.model;
+
+import java.util.List;
+
+public class Company {
+    private String companyName;
+    private int employeesNumber;
+    private List<Employee> employees;
+
+    public Company() {
+    }
+
+    public Company(String companyName, List<Employee> employees) {
+        this.companyName = companyName;
+        this.employeesNumber = employees.size();
+        this.employees = employees;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public int getEmployeesNumber() {
+        return employeesNumber;
+    }
+
+    public void setEmployeesNumber(int employeesNumber) {
+        this.employeesNumber = employeesNumber;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+}
